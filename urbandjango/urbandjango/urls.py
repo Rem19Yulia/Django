1bda7ed9f0296464dbf3e31f6c086aa93cb23cdf
Rem19Yulia <kyv190591@gmail.com>
2025-01-17 19:04:58 +0300

    Update urls.py

--- urbandjango/urbandjango/urls.py
+++ urbandjango/urbandjango/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-import task2
-from task2 import views
-from task2.views import ClassBasedView, FunctionBasedView
+from django.urls import path
+from task3 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ClassBasedView.as_view(), name='ClassBasedView'),
-    path('FunctionBasedView/', views.FunctionBasedView),
+    path('', views.index, name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
 ]
