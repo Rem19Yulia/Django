95e57badbaf4023f73eb2d4d336a04131d15f3a4
Rem19Yulia <kyv190591@gmail.com>
2025-01-20 23:58:01 +0300

    Update urls.py
    task5

--- urbandjango/urbandjango/urls.py
+++ urbandjango/urbandjango/urls.py
@@ -35,3 +35,14 @@ urlpatterns = [
 #    path('cart/', views.cart, name='cart'),
 #    path('games/', views.games_view, name='games'),
 #]
+
+#task5
+#from django.contrib import admin
+#from django.urls import path
+#from task5 import views
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+ #   path('', views.sign_up_by_html, name='sign_up_by_html'),
+#    path('django_sign_up/', views.sign_up_by_django, name='sign_up_by_django'),
+#]
